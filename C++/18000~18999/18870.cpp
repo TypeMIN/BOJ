62e9bd3bfacebc37a7d1e9828bee28933406f22e
TypeMIN <typemin@naver.com>
2022-01-13 00:03:51 +0900

    2022-01-12 study

--- C++/18000~18999/18870.cpp
+++ C++/18000~18999/18870.cpp
@@ -1,50 +1,49 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm> // sort()
 
 using namespace std;
 
-typedef struct {
-  int value;
-  int order;
-  int count;
+typedef struct { // 좌표 구조체
+  int value; // 좌표값
+  int order; // 입력 순서
+  int count; // 압축값
 } Point;
 
 bool cmp_value(Point x1, Point x2) {
   return x1.value < x2.value;
-}
+} // 좌표값 오름차순 정렬
 
 bool cmp_order(Point x1, Point x2) {
   return x1.order < x2.order;
-}
+} // 입력 순서 오름차순 정렬
 
 int main(void) {
-  int N;
-  int count;
-  Point* x;
+  int N; // 좌표의 수
+  int count; // 압축값
+  Point* x; // 좌표 배열
 
   cin >> N;
   x = new Point[N];
   for(int i = 0; i < N; i++) {
-    cin >> x[i].value;
-    x[i].order = i;
+    cin >> x[i].value; // 좌표값 입력
+    x[i].order = i; // 입력 순서 저장
   }
-
+  // 좌표값 오름차순 정렬
   sort(x, x+N, cmp_value);
-
+  
   count = 0;
   x[0].count = count;
   for(int i = 1; i < N; i++) {
-    if(x[i].value > x[i-1].value) {
+    if(x[i].value > x[i-1].value)
       count++;
-    }
+    // 좌표값이 이전 값과 같을 시 압축값 증가 X
     x[i].count = count;
   }
-
+  // 입력 순서 오름차순 정렬
   sort(x, x+N, cmp_order);
-
-  for(int i = 0; i < N-1; i++) {
+  // 압축값 출력
+  for(int i = 0; i < N-1; i++)
     cout << x[i].count << " ";
-  }
   cout << x[N-1].count << '\n';
 
   delete[] x;
