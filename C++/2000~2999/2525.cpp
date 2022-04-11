73f26b70bbc205a66443578ac4d91f01027823ef
TypeMIN <typemin@naver.com>
2022-04-11 21:23:57 +0900

    2022-04-11 study

--- C++/2000~2999/2525.cpp
+++ C++/2000~2999/2525.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main(void) {
-  int hour, minute;
-  int require_hour, require_minute;
+  int hour, minute; // 현재 시각
+  int require_hour, require_minute; // 요리하는데 필요한 시간
 
-  cin >> hour >> minute;
-  cin >> require_minute;
+  cin >> hour >> minute;  // 현재 시각 입력
+  cin >> require_minute;  // 요리하는데 필요한 시간 입력
 
-  require_hour = require_minute/60;
-  for(int i = 0; i < require_hour; i++)
+  require_hour = require_minute/60; // 시간 단위 계산
+  for(int i = 0; i < require_hour; i++) // 시간의 합
     hour++;
-  minute += require_minute%60;
+  minute += require_minute%60;  // 분의 합
 
-  if(minute > 59) {
+  if(minute > 59) { // 60분이 초과했을 때 받아올림
     minute -= 60;
     hour++;
   }
-  if(hour > 23) {
+  if(hour > 23) { // 24시 -> 0시로 전환
     hour %= 24;
   }
 
-  cout << hour << " " << minute << '\n';
+  cout << hour << " " << minute << '\n';  // 종료되는 시각 출력
   return 0;
 }  
